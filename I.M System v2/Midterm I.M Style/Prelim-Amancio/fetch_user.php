d4178caa308ed641e8c3adcad2ae505e1f8198a4
Nielvin Amancio <112628938+Amanciolicious@users.noreply.github.com>
2025-03-11 15:42:35 +0800

    Add files via upload

---
+++ I.M System v2/Midterm I.M Style/Prelim-Amancio/fetch_user.php
@@ -0,0 +1,41 @@
+<?php
+include("dbconnection.php");
+
+$sql = "SELECT * FROM students_tbl";
+$result = $connection->query($sql);
+$output = "";
+
+if ($result) {
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $dateStarted = new DateTime($row['date_started']);
+        $daysOld = $dateStarted->diff(new DateTime())->days;
+        $formattedDateStarted = $dateStarted->format('F j, Y');
+        $daysOldMessage = ($daysOld == 1) ? '1 day old' : "$daysOld days old";
+        $profileImage = (!empty($row['profile_image']) && file_exists($row['profile_image'])) 
+            ? "<img src='{$row['profile_image']}' class='img-thumbnail' style='width: 60px; height: 60px; object-fit: fill;  border: none; box-shadow: 0 0 2px #000;'>"
+            : "<i class='bi bi-person-circle' style='font-size: 2rem;'></i>";
+
+        $output .= "
+            <tr>
+                <td>{$row['assign_id']}</td>
+                <td>{$row['student_name']}</td>
+                <td>{$row['school']}</td>
+                <td>{$row['school_address']}</td>
+                <td>{$row['contact_number']}</td>
+                <td>{$row['coordinator']}</td>
+                <td>{$row['organization']}</td>
+                <td title='Started on {$formattedDateStarted}'>{$daysOldMessage}</td>
+                <td class='text-center'>{$profileImage}</td>
+                <td>
+                    <button class='btn btn-warning btn-sm' onclick='openEditModal({$row['assign_id']})'>Edit</button>
+                    <button class='btn btn-danger btn-sm' onclick='deleteUser({$row['assign_id']})'>Delete</button>
+                </td>
+            </tr>
+        ";
+    }
+} else {
+    $output .= "<tr><td colspan='10' class='text-center'>No records found</td></tr>";
+}
+
+echo $output;
+?>
